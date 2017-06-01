b0416ba610c7e818d5caf321f6806ade1333be0c
ukimiawz <melisa.cecilia.mail@gmail.com>
2017-06-01 19:36:23 +0200

    Remove test parameters in example script

--- example.py
+++ example.py
@@ -1,19 +1,19 @@
 from src.cognigy_client import Cognigy_client 
 
 base_url = "http://api.cognigy.com"
-api_key = "testapikey"
-flow = "starwars"
+api_key = "<your-api-key>"
+flow = "<your-flow>"
 reset_state = True
 reset_context = True
-username = "python-test-username"
-channel = "python-test"
+username = "<your-username>"
+channel = "<your-channel>"
 language = "en-US"
 
 def handle_output_method(result):
-    print 'lalalala'
     print result
 
-test = Cognigy_client(base_url, 3100, username, api_key, channel, flow, language, handle_output=handle_output_method)
-test.connect()
+socket_client = Cognigy_client(base_url, 3100, username, api_key, channel, flow, language, handle_output=handle_output_method)
+# has to connect first before sending message
+socket_client.connect()
 print 'Connected'
-test.send_message('Hi', None)
+socket_client.send_message('Hi', None)
